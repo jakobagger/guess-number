309271f86215b9c240116bdd072b2e7a5946e8c0
jakobagger <113458342+jakobagger@users.noreply.github.com>
2024-01-29 14:06:42 +0100

    empty functions

--- sscript.js
+++ sscript.js
@@ -6,3 +6,15 @@ function start(){
     console.log("up and running");
     
 }
+
+function generateRandomNumber(){}
+
+function receiveGuess(){}
+
+function checkGuess(){}
+
+function guessIsCorrect(){}
+
+function guessIsTooLow(){}
+
+function guessIsTooHigh(){}
